9ff63f9b62d9f9a964dd5014a3428cddafd6e92f
Adedayo100-ux <66147046+Adedayo100-ux@users.noreply.github.com>
2021-10-16 03:44:05 +0100

    Add files via upload

---
+++ js/fireAuth.js
@@ -0,0 +1,21 @@
+// TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyC7nxTSNzkBA9YWhgWPbvdsmCGPY3qJtdI",
+    authDomain: "my-portfolio-96fc3.firebaseapp.com",
+    projectId: "my-portfolio-96fc3",
+    storageBucket: "my-portfolio-96fc3.appspot.com",
+    messagingSenderId: "984349477491",
+    appId: "1:984349477491:web:e74268c3b2c5155ee87806",
+    measurementId: "G-CHR7Y218DR"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
+}
+
+
